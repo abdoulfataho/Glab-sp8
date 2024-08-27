e2edef75766488145ad41ea782f11d5c13c17105
abdoulfataho <yamyaogofataho@gmail.com>
2024-08-27 09:33:46 -0400

    final sp8

---
+++ Glab-SP8/src/main/java/com/example/Glab_SP8/GlabSp8Application.java
@@ -0,0 +1,13 @@
+package com.example.Glab_SP8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlabSp8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlabSp8Application.class, args);
+	}
+
+}
---
+++ Glab-SP8/src/test/java/com/example/Glab_SP8/GlabSp8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Glab_SP8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlabSp8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
